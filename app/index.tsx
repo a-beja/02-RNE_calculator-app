1034015e9297a07223d4ac6256a78deff93699c3
Abril <abrilm.dev@gmail.com>
2026-06-14 19:03:46 -0600

    ExpoRouter: index and _layout

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>CalculatorApp</Text>
+    </View>
+  )
+}
+export default CalculatorApp;
