ca6ad53ce9cac0e2bebc6c4cfb4340bfa939acea
Abril <abrilm.dev@gmail.com>
2026-06-16 17:41:37 -0600

    Enhance layout and theme: Added font loading in _layout, updated background color, and improved text styling in index. Defined color constants in theme.

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native';
 const CalculatorApp = () => {
   return (
     <View>
-      <Text>CalculatorApp</Text>
+      <Text style= {{ fontSize: 50, fontFamily: 'SpaceMono', color: 'white' }}>
+        CalculatorApp
+      </Text>
     </View>
   )
 }
