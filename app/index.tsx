56b2137442a9771885771664f2142524f735f8a3
Abril <abrilm.dev@gmail.com>
2026-06-23 23:35:05 -0600

    Refactor CalculatorApp to integrate useCalculator hook for dynamic formula display and update button press handling to build numbers, enhancing interactivity and functionality.

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,23 @@
 import CalculatorButton from '@/components/CalculatorButton';
 import ThemeText from '@/components/ThemeText';
+import { useCalculator } from '@/hooks/useCalculator';
 import { globalStyles } from '@/styles/global-styles';
 import { View } from 'react-native';
 
 
 const CalculatorApp = () => {
+
+  const {
+    formula,
+    buildNumber
+  } = useCalculator();
+
   return (
     <View style={ globalStyles.calculatorContainer }>
       
       {/* Resultados */}
       <View style={{ marginBottom: 20 }}>
-        <ThemeText variant='h1'>50 x 500</ThemeText>
+        <ThemeText variant='h1'>{ formula }</ThemeText>
         <ThemeText variant='h2'>250000000000</ThemeText>
       </View>
 
@@ -23,26 +30,26 @@ const CalculatorApp = () => {
           <CalculatorButton label='÷' color ='orange' onPress={() => console.log('!')}/>
         </View>
         <View style={ globalStyles.row }>
-          <CalculatorButton label='7' onPress={() => console.log('7')}/>
-          <CalculatorButton label='8' onPress={() => console.log('8')}/>
-          <CalculatorButton label='9' onPress={() => console.log('9')}/>
+          <CalculatorButton label='7' onPress={() => buildNumber('7')}/>
+          <CalculatorButton label='8' onPress={() => buildNumber('8')}/>
+          <CalculatorButton label='9' onPress={() => buildNumber('9')}/>
           <CalculatorButton label='x' color='orange' onPress={() => console.log('x')}/>
         </View>
         <View style={ globalStyles.row }>
-          <CalculatorButton label='4' onPress={() => console.log('4')}/>
-          <CalculatorButton label='5' onPress={() => console.log('5')}/>
-          <CalculatorButton label='6' onPress={() => console.log('6')}/>
+          <CalculatorButton label='4' onPress={() => buildNumber('4')}/>
+          <CalculatorButton label='5' onPress={() => buildNumber('5')}/>
+          <CalculatorButton label='6' onPress={() => buildNumber('6')}/>
           <CalculatorButton label='-' color = 'orange' onPress={() => console.log('-')}/>
         </View>
         <View style={ globalStyles.row }>
-          <CalculatorButton label='1' onPress={() => console.log('1')}/>
-          <CalculatorButton label='2' onPress={() => console.log('2')}/>
-          <CalculatorButton label='3' onPress={() => console.log('3')}/>
+          <CalculatorButton label='1' onPress={() => buildNumber('1')}/>
+          <CalculatorButton label='2' onPress={() => buildNumber('2')}/>
+          <CalculatorButton label='3' onPress={() => buildNumber('3')}/>
           <CalculatorButton label='+' color ='orange' onPress={() => console.log('+')}/>
         </View>
         <View style={ globalStyles.row }>
-          <CalculatorButton label='0' doubleSize={true} onPress={() => console.log('0')}/>
-          <CalculatorButton label='.' onPress={() => console.log('.')}/>
+          <CalculatorButton label='0' doubleSize={true} onPress={() => buildNumber('0')}/>
+          <CalculatorButton label='.' onPress={() => buildNumber('.')}/>
           <CalculatorButton label='=' color ='orange' onPress={() => console.log('=')}/>
         </View>
       </View>
