441bb646a0b7785e2f52ad310a61bcfb8ab6afdc
Abril <abrilm.dev@gmail.com>
2026-09-19 04:00:12 -0600

    Refactor useCalculator hook to improve buildFormula logic and add getLastNumber function for better formula handling.

--- hooks/useCalculator.tsx
+++ hooks/useCalculator.tsx
@@ -5,6 +5,11 @@ export const useCalculator = () => {
     const [formula, setFormula] = useState('0');
 
 
+    const getLastNumber = (): string => {
+        const parts = formula.split(/[+\-x÷]/);
+        return parts[ parts.length - 1 ];
+    }
+
     const clean = () => {
         setFormula('0');
     }
@@ -17,13 +22,33 @@ export const useCalculator = () => {
         }
     }
 
-    const buildFormula = (formulaString: string) => {
-        if( formula === '0' && formulaString !== '.') return setFormula(formulaString);
-        setFormula(formula + formulaString);
+    const buildFormula = ( newDigit: string ) => {
+        let lastNumber = getLastNumber();
+        
+        if( lastNumber === '0' && newDigit !== '.'){
+            console.log('hola, empiezo con cero y no puse un punto');
+            setFormula(formula.slice(0, -1) + newDigit);
+            return;
+        }
+
+        if( lastNumber === '' && newDigit === '.'){
+            console.log('hola, lastnumer es vacio y puse un punto');
+            return setFormula( formula + '0' + newDigit );
+        }
+
+        if( lastNumber.includes('.') && newDigit === '.' ){
+            console.log('hola, ya tengo un punto y quiero poner otro');
+            return;
+        }
+
+        if( lastNumber === '0' && newDigit === '0') return;
+        
+        setFormula( formula + newDigit );
     }
 
     return {
         formula,
+
         buildFormula,
         clean,
         deleteLast,
