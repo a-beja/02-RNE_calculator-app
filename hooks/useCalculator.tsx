97c9ebafc48a5a291becbcc3b74fb1a720d56319
Abril <abrilm.dev@gmail.com>
2026-09-19 04:43:08 -0600

    Fix useEffect dependency array in useCalculator hook to ensure proper result calculation

--- hooks/useCalculator.tsx
+++ hooks/useCalculator.tsx
@@ -13,7 +13,7 @@ export const useCalculator = () => {
         if( formula !== '0' ){
             calculateResult();
         }
-    }, )
+    }, [formula] );
 
 
     const getLastNumber = (): string => {
